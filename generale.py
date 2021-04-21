7049a559e25859ae4349edb4daa41d4a3eaf732b
Giuseppina3 <gdenicolo03@gmail.com>
2021-04-21 15:24:50 +0200

    Passo secondo

--- generale.py
+++ generale.py
@@ -5,16 +5,34 @@ class Auto:
         self.cavalli = cavalli_p
         self.produttore = produttore_p
         self.tipo = tipo_p
-
 comando = input('Inserisci comando')
 comando = int(comando)
 
 lista = []
-lista_test = [ Auto ('x', 10, 5, 'y', 'u'), Auto('a', 5, 50, 'b', 'c')]
+lista_test = [ Auto(), Auto()]
+lista_test[0].tipo='u'
+lista_test[0].cavalli=5
+lista_test[0].modello = 'x'
+lista_test[0].produttore ='x'
+lista_test[0].cilindrata = 20
+
+lista_test[1].tipo= 'b'
+lista_test[1].cavalli= 50
+lista_test[1].modello = 'y'
+lista_test[1].produttore = 'x'
+lista_test[1].cilindrata = 30
+
 
 while comando != -1:
-    if comando==1 :#lettura
-        print()
+    if comando==1 :               #lettura
+        for p in lista_test:
+            print('Modello'+ p.modello)
+            print('Cilindrata'+ str(p.cilindrata))
+            print('Cavalli'+ str(p.cavalli))
+            print('Produttori' + p.produttore)
+            print ('Tipo' + p.tipo)
+            print(p.modello, str(p.cilindrata)+ '', str(p.cavalli) + '', p.produttore, p.tipo)
+
 
     if comando==2 :#ricerca
         print()
